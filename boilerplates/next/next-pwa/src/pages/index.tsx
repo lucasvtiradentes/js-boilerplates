97e92fa63f2cb53b2e24a620a187a9f844258794
lucasvtiradentes <lucasvtiradentes@gmail.com>
2023-02-26 22:50:19 -0300

    :sparkles: feature: add next pwa boilerplate (#16)
    add next pwa boilerplate

---
+++ boilerplates/next/next-pwa/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Header } from '../components/Header';
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <p>Hello world ai galeraaaaa</p>
+    </>
+  );
+};
+
+export default Home;
